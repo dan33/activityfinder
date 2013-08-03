3803b935f1154363b3438c79149464ded11a5a86
Jack Jeffress <jackjeffress@gmail.com>
2013-08-03 19:45:41 +1000

    added activity save into controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,5 @@
 class ActivitiesController < ApplicationController
+	before_filter :authenticate_user!
 
 	def new
 		@activity = Activity.new
@@ -7,10 +8,12 @@ class ActivitiesController < ApplicationController
 	def create
 		@user = current_user
 		@activity = Activity.new(params[:activity])
-		# Not sure of the way to do this..  This creation is pushing into the membership fine, we need to find the membership where the activity id is equal to the one that was just created (after the save) and then push the role into that rather than through the user/
+		# Not sure of the way to do this..  This creation is pushing into the membership fine, we need to find the membership where the activity id is equal to the one that was just created (after the save) and then push the role into that rather than through the user
 		if @activity.save
 			@user.activities << @activity
 			@activity.memberships.first.role = 'owner'
+			binding.pry
+			@activity.save
 			redirect_to @activity
 
 			# Why doesn't this work Joel?  I have a feeling I stuffed the syntax but ya no what I mn
