8581d631dfdfd721f855dbf9df36c230fef47861
Obi <andrew.obrien1@gmail.com>
2013-08-03 12:58:05 +1000

    fix activity controller due to error in last commit

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,26 +1,26 @@
 class ActivitiesController < ApplicationController
 
-  def new
-  end
+	def new
+	end
 
-  def create
-    @user = current_user
-    @activity = Activity.new(params[:activity])
-    @user.activities << @activity
-    @activity.memberships.first.role = 'owner'
-      if @activity.save
-        redirect_to activities_path(@activity)
-      else
-        render :new
-      end
-  end
+	def create
+		@user = current_user
+		@activity = Activity.new(params[:activity])
+			if @activity.save
+				@user.activities << @activity
+				@activity.memberships.first.role = 'owner'
+				redirect_to activities_path(@activity)
+			else
+				render :new
+			end
+	end
 
-  def index
-    @activities = Activity.all
-  end
+	def index
+		@activities = Activity.all
+	end
 
-  def show
-    @activity = Activity.find(params[:id])
-  end
+	def show
+		@activity = Activity.find(params[:id])
+	end
 
 end
