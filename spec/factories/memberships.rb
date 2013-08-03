50cd5bce7adb5d601c7912e6eeaf6aaedf99231e
Obi <andrew.obrien1@gmail.com>
2013-08-03 14:32:08 +1000

    create controller

--- spec/factories/memberships.rb
+++ spec/factories/memberships.rb
@@ -2,5 +2,20 @@
 
 FactoryGirl.define do
   factory :membership do
+
+  		describe "POST #create" do
+  			let (:user) { FactoryGirl.create(:user)}
+  			sign_in user
+
+    		context "with valid attributes" do
+      		it "creates a new membership"
+      		it "has a role of member"
+    		end
+
+    		context "with invalid attributes" do
+      		it "does not save the new contact in the database"
+      		it "re-renders the :new template"
+    		end
+  		end
   end
 end
