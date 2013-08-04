72ed77a404747ec996954749e63a389eb3f56577
Obi <andrew.obrien1@gmail.com>
2013-08-04 14:44:27 +1000

    start leaflet.js map implementation

---
+++ app/assets/javascripts/activityfinder.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+  $('#search').on
+
+
+  var map = L.map('map').setView([51.505, -0.09], 13);
+  var z = 13
+
+  var calculate_map_tiles = function() {
+    n = 2 ^ zoom
+    x = ((-0.09 + 180) / 360) * n
+    y = (1 - (ln(tan(51.505) + sec(51.505)) / Pi)) / 2 * n
+  }
+
+
+  L.tileLayer('http://{s}.tile.cloudmade.com/979aecf591d149b3a4d1e9dabdd52998/997/256/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+  }).addTo(map);
+});
--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,22 +1,22 @@
 class MembershipsController < ApplicationController
-	def create
-		@activity = Activity.find(params[:activity_id])
-		@membership = current_user.memberships.create(:activity_id => @activity.id)
-		if @membership.save
-			redirect_to @activity
-		else
-						#render error messages partial
-		end
-	end
+def create
+  @activity = Activity.find(params[:activity_id])
+  @membership = current_user.memberships.create(:activity_id => @activity.id)
+  if @membership.save
+    redirect_to @activity
+  else
+    #render error messages partial
+  end
+end
 
-	def new
-	end
+def new
+end
 
-	def destroy
-		@membership = current_user.memberships.find(params[:id])
-  		@membership.destroy
-		flash[:notice] = "Removed membership."
-    	redirect_to root_path
-	end
+def destroy
+  @membership = current_user.memberships.find(params[:id])
+  @membership.destroy
+  flash[:notice] = "Removed membership."
+  redirect_to root_path
+end
 
 end
