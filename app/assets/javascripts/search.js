3004dfcf61dfc7b83df50e242e29783e98944eef
Jack Jeffress <jackjeffress@gmail.com>
2013-08-04 19:33:10 +1000

    added find functionality for the map

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,16 +1,40 @@
 $(document).ready(function() {
- $('#search_submit').click(function() {
-  var search_result = $('#search_field').val();
-  $.ajax({
-   dataType: 'json',
-   data: {
-    'address': search_result
-   },
-   type: 'POST',
-   url: '/search/',
-  }).done(function(data) {
-   latlong = data;
-   console.log(latlong);
+  $('#search_submit').click(function() {
+   var search_result = $('#search_field').val();
+   $.ajax({
+    dataType: 'json',
+    data: {
+     'address': search_result
+    },
+    type: 'POST',
+    url: '/search/',
+   }).done(function(data) {
+    var latlong = data;
+    console.log(latlong);
+    $('#map_float').empty();
+    $('#map_float').append('<div id="map"/>');
+    var map = L.map('map', {
+     center: new L.LatLng(latlong[0], latlong[1]),
+     zoom: 4,
+     // layers: [cities]
+    });
+    var z = 13;
+
+    // // MARKR TEMPLATE
+    // L.marker([51.5, -0.09]).addTo(map)
+    //   .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+    //   .openPopup();
+    // //
+
+    var calculate_map_tiles = function() {
+     n = 2 ^ zoom;
+     x = ((-0.09 + 180) / 360) * n;
+     y = (1 - (ln(tan(51.505) + sec(51.505)) / Pi)) / 2 * n;
+    };
+
+    L.tileLayer('http://{s}.tile.cloudmade.com/979aecf591d149b3a4d1e9dabdd52998/997/256/{z}/{x}/{y}.png', {
+     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+    }).addTo(map);
+   });
   });
- });
 });
