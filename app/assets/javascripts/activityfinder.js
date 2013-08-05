5c266a8f186134094048c8cc39bdbf517dfbb3cb
Obi <andrew.obrien1@gmail.com>
2013-08-05 15:09:08 +1000

    get search functionality and pins redraw working

--- app/assets/javascripts/activityfinder.js
+++ app/assets/javascripts/activityfinder.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
     url: '/activities/',
   }).done(function(data) {
     var layers = [];
-      var pins = [];
+    var pins = [];
 
-    var process_category = function (c) {
+    var process_category = function(c) {
       pins = [];
       _.each(c.activities, process_pin);
       layers.push({
@@ -19,30 +19,31 @@ $(document).ready(function() {
       });
     }
 
-     var process_pin = function(activity) {
+    var process_pin = function(activity) {
       var pin = L.marker([activity.latitude, activity.longitude]).bindPopup('<h3>' + activity.title + '</h3>' + '<br>' + activity.description);
       pins.push(pin);
-  };
+      console.log(pins)
+    };
 
-  _.each(data, process_category);
-  // debugger;
+    _.each(data, process_category);
+    // debugger;
 
 
     var map = L.map('map', {
-    center: new L.LatLng(-33.8674869, 151.2069902),
-    zoom: 10,
-    layers: [layers[0].layer,layers[1].layer,layers[2].layer]
+      center: new L.LatLng(-33.8674869, 151.2069902),
+      zoom: 10,
+      layers: [layers[0].layer, layers[1].layer, layers[2].layer]
     });
 
-      // var z = 13;
+    // var z = 13;
     var calculate_map_tiles = function() {
-    n = 2 ^ zoom;
-    x = ((-0.09 + 180) / 360) * n;
-    y = (1 - (ln(tan(51.505) + sec(51.505)) / Pi)) / 2 * n;
-  };
+      n = 2 ^ zoom;
+      x = ((-0.09 + 180) / 360) * n;
+      y = (1 - (ln(tan(51.505) + sec(51.505)) / Pi)) / 2 * n;
+    };
     L.tileLayer('http://{s}.tile.cloudmade.com/979aecf591d149b3a4d1e9dabdd52998/997/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-  }).addTo(map);
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+    }).addTo(map);
   });
 
 });
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -9,14 +9,33 @@ $(document).ready(function() {
       type: 'POST',
       url: '/search/',
     }).done(function(data) {
+      var layers = [];
+      var pins = [];
       var latlong = data.latlong;
-      var activities = data.activities;
+
+      var process_category = function(c) {
+        pins = [];
+        _.each(c.activities, process_pin);
+        layers.push({
+          id: c.id,
+          title: c.title,
+          layer: L.layerGroup(pins)
+        });
+      }
+
+      var process_pin = function(activity) {
+        var pin = L.marker([activity.latitude, activity.longitude]).bindPopup('<h3>' + activity.title + '</h3>' + '<br>' + activity.description);
+        pins.push(pin);
+      };
+
+      _.each(data.cats, process_category);
+
       $('#map_float').empty();
       $('#map_float').append('<div id="map"/>');
       var map = L.map('map', {
         center: new L.LatLng(latlong[0], latlong[1]),
         zoom: 10,
-        // layers: [cities]
+        layers: [layers[0].layer, layers[1].layer, layers[2].layer]
       });
 
       var calculate_map_tiles = function() {
@@ -25,18 +44,12 @@ $(document).ready(function() {
         y = (1 - (ln(tan(latlong[1]) + sec(latlong[1])) / Pi)) / 2 * n;
       };
 
-      var process_activities = function(a) {
-        _.each(a, function(a) {
-          L.marker([a.latitude, a.longitude]).addTo(map)
-            .bindPopup('<h3>' + a.title + '</h3>' + '<br>' + a.description);
-        });
-      };
+
 
       L.tileLayer('http://{s}.tile.cloudmade.com/979aecf591d149b3a4d1e9dabdd52998/997/256/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
       }).addTo(map);
 
-      process_activities(activities);
     });
   });
 });
