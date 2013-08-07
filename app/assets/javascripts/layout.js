64d0ea56ebf3258de5f2fadfc38c2ed3818065e8
Jack Jeffress <jackjeffress@gmail.com>
2013-08-07 15:41:21 +1000

    almost finished modelling

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,19 +1,30 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    if (window.location.pathname != "/" ) { $('#panel').addClass("panel_show", 0 ) };
+    if (window.location.pathname != "/") {
+        $('#panel').addClass("panel_show", 0);
+    }
 
-    $('#panelactivities, #panelme').click(function () {
-        $('#panel').toggleClass("panel_show", 400 );
+    $('#panelactivities, #panelme').click(function() {
+        $('#panel').toggleClass("panel_show", 400);
     });
 
-
-    $('#panelsearch').keypress(function(e){
-    if (e.which == 13){
-        $("#search_submit").click();
+    $('#panelsearch').keypress(function(e) {
+        if (e.which == 13) {
+            $("#search_submit").click();
         }
     });
 
     $('leaflet-control-layers').addClass("leaflet-control-layers-expanded");
 
+    $('.login').click(function() {
+        $('#modal').hide();
+        $('.log_in').show();
+        // $('.block_out').fadeOut(100);
+    });
 
+    $('.signup').click(function() {
+        $('#modal').hide();
+        $('.sign_up').show();
+        // $('.block_out').fadeOut(100);
+    });
 });
