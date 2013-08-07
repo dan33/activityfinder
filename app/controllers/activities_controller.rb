f42e93ac38631b45a740ec554eeaf99864ac5e16
Dan Addington <daniel.anthony.addington@gmail.com>
2013-08-08 09:42:37 +1000

    removed activity.users json from activities controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -20,33 +20,19 @@ class ActivitiesController < ApplicationController
     @activities = Activity.all
     @categories = Category.all
     cats = []
-    act = []
 
     @categories.each do |category|
       cats.push({
-        :id => category.id,
-        :title => category.title,
-        :activities => category.activities,
-        :image => category.image.url
-      })
-    end
-
-    @activities.each do |activity|
-      act.push({
-        :id => activity.id,
-        :title => activity.title,
-        :users => activity.users
-      })
+          :id => category.id,
+          :title => category.title,
+          :activities => category.activities,
+          :image => category.image.url
+        })
     end
 
     respond_to do |format|
       format.html
-      format.json {
-        render :json => {
-          :cats => cats,
-          :act => act
-          }
-        }
+      format.json { render :json => cats }
     end
   end
 
