760ed07a29f68af104aa80c5a6af7c97f5c68e6c
Jack Jeffress <jackjeffress@gmail.com>
2013-08-08 11:21:18 +1000

    rainbow styling

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -14,22 +14,51 @@ $(document).ready(function() {
         }
     });
 
-    // $('.activity').hover(function () {
-    //     $(this).addClass('white');
-    // });
-
     $('#button2').click(function() {
         $('#panel').toggle( "blind" );
     });
 
     $('.login').click(function() {
         $('#modal').hide();
+        $('#container').hide();
         $('.log_in').show();
         // $('.block_out').fadeOut(100);
     });
 
+    $('#signuph2').click(function() {
+        $('#modal').hide();
+        $('#container').hide();
+        $('.sign_up').show();
+        // $('.block_out').fadeOut(100);
+    });
+
+    $('#loginh2').click(function() {
+        $('#modal').hide();
+        $('#container').hide();
+        $('.sign_up').hide();
+        // $('.block_out').fadeOut(100);
+    });
+
+    $('#close').click(function () {
+        $('#modal').hide();
+        $('.sign_up').hide();
+        $('.block_out').hide();
+        $('.log_in').hide();
+    });
+
+    $('#closelogin').click(function () {
+        $('.log_in').hide();
+        $('.block_out').hide();
+    });
+
+    $('#closesignup').click(function () {
+        $('.sign_up').hide();
+        $('.block_out').hide();
+    });
+
     $('.signup').click(function() {
         $('#modal').hide();
+        $('#container').hide();
         $('.sign_up').show();
         // $('.block_out').fadeOut(100);
     });
