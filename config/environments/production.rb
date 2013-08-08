9634476257dbae86096213fb4c1d540352bee4e0
Dan Addington <daniel.anthony.addington@gmail.com>
2013-08-08 12:08:05 +1000

    set asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,9 @@ ActivityFinder::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  #send images to AWS
+  config.action_controller.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
