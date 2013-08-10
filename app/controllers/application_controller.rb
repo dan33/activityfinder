c57db6b53d02cc0084467c5b24cf472ba05f0bf2
Obi <andrew.obrien1@gmail.com>
2013-08-10 21:51:07 +1000

    add member? and owner? helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :member?, :owner?
+
+  def member?
+    @activity = Activity.find_by_slug(params[:id])
+    @activity.users.include?(current_user)
+  end
+
+  def owner?
+    @activity = Activity.find_by_slug(params[:id])
+    current_user.memberships.where(:activity_id => @activity.id).first.role == 'owner'
+  end
 
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -31,20 +31,17 @@ class Activity < ActiveRecord::Base
 
   def geocode
     #over API query limit fix
-    self.latitude = 32.3456
-    self.longitude = 141.4346
+    # self.latitude = 32.3456
+    # self.longitude = 141.4346
 
-    # result = Geocoder.search(self.address).first
-    #   if result.present?
-    #     self.latitude = result.latitude
-    #     self.longitude = result.longitude
-    #   end
+    result = Geocoder.search(self.address).first
+      if result.present?
+        self.latitude = result.latitude
+        self.longitude = result.longitude
+      end
   end
 
-  def is_owner
+  def owner_name
     self.memberships.where(:role => 'owner').first.user.name
   end
-
-
-
 end
